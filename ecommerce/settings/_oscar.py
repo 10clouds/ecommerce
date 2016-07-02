e91575cbc1bf41991d2365f019658f9be108dfa8
Sergey Kospanov <sergey.kospanov@gmail.com>
2016-07-02 13:25:07 +0300

    Revert "braintree payment page + processor"

--- ecommerce/settings/_oscar.py
+++ ecommerce/settings/_oscar.py
@@ -102,7 +102,6 @@ OSCAR_DEFAULT_CURRENCY = 'USD'
 PAYMENT_PROCESSORS = (
     'ecommerce.extensions.payment.processors.cybersource.Cybersource',
     'ecommerce.extensions.payment.processors.paypal.Paypal',
-    'ecommerce.extensions.payment.processors.braintree.Braintree',
 )
 
 PAYMENT_PROCESSOR_RECEIPT_PATH = '/commerce/checkout/receipt/'
