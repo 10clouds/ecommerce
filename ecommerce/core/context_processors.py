f4790c92f2400a880f09862109c407a7c6bdc28f
inlanger <inlanger@gmail.com>
2016-11-23 01:42:28 +0200

    update ecommerce basket page

--- ecommerce/core/context_processors.py
+++ ecommerce/core/context_processors.py
@@ -5,6 +5,7 @@ from ecommerce.core.url_utils import get_lms_dashboard_url, get_lms_url
 
 def core(request):
     return {
+        'edevate_base_url': settings.EDEVATE_BASE_URL,
         'lms_base_url': get_lms_url(),
         'lms_dashboard_url': get_lms_dashboard_url(),
         'platform_name': request.site.name,
